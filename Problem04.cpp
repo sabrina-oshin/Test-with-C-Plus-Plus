87f87dcca396048221d8cfce261d820387db0764
Sabrina Chowdhury <50597189+sabrina-oshin@users.noreply.github.com>
2021-02-25 03:15:46 +0100

    Problem04 has been modified

---
+++ Problem04.cpp
@@ -0,0 +1,49 @@
+// Problem 04:
+// If 1<= n <= 9, print the lowercase English word corresponding to the number (e.g., one for 1, two for 2, etc.)
+// If n>9, print Greater than 9
+// The code starts here:
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+int n ;
+cin >> n;
+if (n >= 1 && n <= 9)
+{
+switch (n) {
+  case 1:
+    cout << "one";
+    break;
+  case 2:
+    cout << "two";
+    break;
+  case 3:
+    cout << "three";
+    break;
+  case 4:
+    cout << "four";
+    break;
+  case 5:
+    cout << "five";
+    break;
+  case 6:
+    cout << "six";
+    break;
+  case 7:
+    cout << "seven";
+    break;
+  case 8:
+    cout << "eight";
+    break;
+  case 9:
+    cout << "nine";
+    break;
+}
+}
+else
+{
+cout << "Greater than 9";
+}
+}
